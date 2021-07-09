9ea56eef643625629eb894b066ecb1fa744143df
Boumarouane <i.boumarouane@gmail.com>
2021-07-09 15:57:57 +0200

    Loader en three.js

---
+++ app.js
@@ -0,0 +1,65 @@
+// Variables
+var scene, camera, renderer, material, geometry, mesh, dae, collada, malha, loader;
+
+// ! initialisation
+const init = () => {
+
+    let container = document.querySelector('#container');
+
+    // Configuration renderer
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    container.appendChild( renderer.domElement );
+    
+    // Configuration camera
+    let fov = 30;
+    let aspect = 2;
+    let near = 0.1;
+    let far = 2000;
+    camera = new THREE.PerspectiveCamera(fov,aspect, near, far);
+    camera.position.set(0, 0, 30);  // (x, y, z)
+    camera.lookAt(0, 0, 0);
+
+    // Configuration Scene
+    scene = new THREE.Scene();
+
+    // ColladaLoader
+
+    // const loader = new THREE.ColladaLoader();
+	// 		loader.load( './terence-collada-max/source/2314159fe94440d5af1c1ff22107e06f.dae.dae', function ( collada ) {
+	// 		let dae = collada.scene;
+    //         scene.add(dae)
+    //         dae.position.set(0,0,0)
+
+    //         });
+
+    // GLTF Loader
+
+    loader = new THREE.GLTFLoader();
+    loader.load("./chuck_collada_test2/scene.gltf", function(object) {
+        let model = object.scene;
+        model.position.set(0, 0, 0);
+        scene.add(model);
+    });
+
+    // Config light
+   let light = new THREE.AmbientLight(0xffffff, 1);
+    scene.add(light);
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, 2 );
+    directionalLight.position.set( 1, 100, 0 );
+    scene.add( directionalLight );
+
+    // config orbitcontrols 
+    let controls = new THREE.OrbitControls(camera, renderer.domElement);
+}
+
+
+// ! Animation
+const animate = () => {    
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+init();
+animate();
